4b374adc479b14df3b2aa9f88a3aed5053911a45
jacob <jacobochieng71@gmail.com>
2020-04-03 12:28:01 +0300

    this commit will add changes to the script in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var CC = century digits
-var  YY = Year digits
-var MM = Month
-var  DD = Day of the month
-var mod = modulus function
+var CC = 'century digits'
+var  YY = 'Year digits'
+var MM = 'Month'
+var  DD = 'Day of the month'
+var mod = 'modulus function'
 var number1 = parseInt(prompt("Enter a birthday:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
