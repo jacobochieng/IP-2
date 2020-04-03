0afac1640557fee863d7ba965944d82629c080dc
jacob <jacobochieng71@gmail.com>
2020-04-03 17:47:08 +0300

    this commit will affect the functions in java script

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,5 @@ alert(result);
 participants.forEach(function(male){
      console.log(male)
 })
-return {Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+return {Day of the week (d) = ( ( ('CC'/4) -2*'CC'-1) + ((5*'YY'/4) ) + ((26*('MM'+1)/10)) + 'DD' ) 'mod' 7
 }
