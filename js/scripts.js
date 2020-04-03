b8cd7ccafca2355749e372ad889b4110b2083102
jacob <jacobochieng71@gmail.com>
2020-04-03 15:33:44 +0300

    this commit will change the functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-const male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var participants = [male,female]
+var male = {"Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"};
 var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 var male = 'male'
 var CC = 'century digits'
@@ -11,6 +8,9 @@ var MM = 'Month'
 var  DD = 'Day of the month'
 var mod = 'modulus function'
 var number1 = parseInt(prompt("Enter a birthday:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+var result = add(number1);
 alert(result);
+participants.forEach(function(male){
+     console.log(male)
+})
+function 
