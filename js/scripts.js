94a3dab24846741304f426bf599e8a34c5e73508
jacob <jacobochieng71@gmail.com>
2020-04-08 12:54:50 +0300

    addition of a new formular

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
-var participants = [male,female]
-var male = {"Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"};
-var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-var male = 'male'
-var Day of the week = "d"
-var CC = 'century digits'
-var  YY = 'Year digits'
-var MM = 'Month'
-var  DD = 'Day of the month'
-var mod = 'modulus function'
-var name = parseInt(prompt("Enter a birthday:"));
-var result = add(name);
-alert(result);
-participants.forEach(function(male){
-     console.log(male)
-})
-return {Day of the week (d) = ( ( ('CC'/4) -2*'CC'-1) + ((5*'YY'/4) ) + ((26*('MM'+1)/10)) + 'DD' ) 'mod' 7
+function validate ()
+var CC = parseInt(document.getElementById("century").value);
+var YY = parseInt(document.getElementById("year").value);
+var MM = parseInt(document.getElementById("month").value);
+var DD = parseInt(document.getElementById("date").value);
+
+if  (CC =="" || CC > 20 || CC < 19){
+     alert("only the 20th and the 19th centuery are allowed")
+     return false;
+
+}    else if (YY == "" || YY > 99 || YY < 0) {
+     alert("Only years between 0 and 99 can be entered")
+     return false;
+
+}    else if ( DD == "" || DD > 31 || DD < 1) {
+     alert("Enter a date between 1 and 31");
+     return false;
+
+}    else {
+     return true;
+     
 }
-function=(d<=0) or (d>31)
-function=(m<= 0) or (m > 12)  
-var participants = ("Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama")
